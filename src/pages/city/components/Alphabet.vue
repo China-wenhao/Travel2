e12a8bc4cdc3efe74add58b9db29097c8b95c261
wenhao <1746594617@qq.com>
2022-08-24 19:11:45 +0800

    add List Alphabet 2022/8/24

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,31 @@
+<template>
+    <ul class="list" >
+        <li class="item" v-for="(item,key) in cities" :key="key">{{key}}</li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name:"CityAlphabet",
+        props:{
+            cities:Object
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+.list
+        display flex
+        justify-content center
+        flex-direction column
+
+        position absolute
+        top 1.58rem
+        right 0
+        bottom 0
+        width .4rem
+        .item
+            line-height .44rem
+            text-align center
+            color #00bcd4
+</style>
